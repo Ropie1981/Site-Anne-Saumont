556b930a5550fa69200b7144ff9351a0b5482ec7
Ropie1981 <109680232+Ropie1981@users.noreply.github.com>
2022-10-27 18:22:26 +0200

    Add files via upload

---
+++ Site Anne Saumont/script.js
@@ -0,0 +1,48 @@
+let toggle = document.querySelector(".toggle");
+let body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+  body.classList.toggle("open");
+});
+
+let img__slider = document.getElementsByClassName("img__slider");
+
+let etape = 0;
+
+let nbr__img = img__slider.length;
+
+let precedent = document.querySelector(".precedent");
+let suivant = document.querySelector(".suivant");
+
+function enleverActiveImages() {
+  for (let i = 0; i < nbr__img; i++) {
+    img__slider[i].classList.remove("active");
+  }
+}
+
+suivant.addEventListener("click", function () {
+  etape++;
+  if (etape >= nbr__img) {
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
+});
+
+precedent.addEventListener("click", function () {
+  etape--;
+  if (etape < 0) {
+    etape = nbr__img - 1;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
+});
+
+setInterval(function () {
+  etape++;
+  if (etape >= nbr__img) {
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
+}, 5000);
